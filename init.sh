dfa7cd152bb19e6c48730fbf8a0890262436867c
Nitin Issac Joy <nijoy@microsoft.com>
2021-03-19 20:51:06 +0530

    asd

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
 printenv
-sudo apt-get -qq update
-sudo apt-get -qq install -y default-jre wget
+apt-get -qq update
+apt-get -qq install -y default-jre wget
 wget -q https://mirrors.estointernet.in/apache//jmeter/binaries/apache-jmeter-5.4.1.tgz
 tar -xf apache-jmeter-5.4.1.tgz
 wget -q https://cloudnativetestingprpsa.blob.core.windows.net/test/SampleApp2.jmx
 
-if [[ "$AZ_BATCH_IS_CURRENT_NODE_MASTER" != "true" ]]
+if [ "$AZ_BATCH_IS_CURRENT_NODE_MASTER" != "true" ];
 then
 ./apache-jmeter-5.4.1/bin/jmeter-server -Jserver.rmi.ssl.disable=true &
 fi
 
-if [[ $1 == "run" ]]
+if [ $1 == "run" ];
 then
     nodes_list = $(echo -e "$AZ_BATCH_NODE_LIST" | tr ';' ',')
     echo $nodes_list
