f71beb8e952aafa09c254cc7352b961b4fac083d
Nitin Issac Joy <nijoy@microsoft.com>
2021-03-19 19:17:06 +0530

    mod

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
-apt-get update
-apt-get install wget
+sudo apt-get update
+sudo apt-get install wget
 wget https://mirrors.estointernet.in/apache//jmeter/binaries/apache-jmeter-5.4.1.tgz
 tar -xvf apache-jmeter-5.4.1.tgz
+wget https://cloudnativetestingprpsa.blob.core.windows.net/test/SampleApp2.jmx
+
 ./apache-jmeter-5.4.1/bin/jmeter-server -Jserver.rmi.ssl.disable=true &
-if [ -z ${AZ_BATCH_IS_CURRENT_NODE_MASTER}]
-then 
-else
+
+if [ $1 == "run"]
+then
     nodes_list = $(echo -e "$AZ_BATCH_NODE_LIST" | tr ';' ',')
     echo $nodes_list
     wget https://cloudnativetestingprpsa.blob.core.windows.net/test/SampleApp2.jmx
     ./apache-jmeter-5.4.1/bin/jmeter -n -t SampleApp2.jmx -R $nodes_list
+fi
