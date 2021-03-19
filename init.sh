cf3ce2928fb4caeecd0d3f28782c4effce87edc6
Nitin Issac Joy <nijoy@microsoft.com>
2021-03-19 21:40:37 +0530

    bug

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo apt-get -qq update
 sudo apt-get -qq install -y default-jre wget
 wget -q https://mirrors.estointernet.in/apache//jmeter/binaries/apache-jmeter-5.4.1.tgz
 tar -xf apache-jmeter-5.4.1.tgz
-wget -q https://cloudnativetestingprpsa.blob.core.windows.net/test/SampleApp2.jmx
+wget -q https://cloudnativetestingprpsa.blob.core.windows.net/test/202102191030128944_HighScaleSampleApp1_HighScaleSampleApp.jmx
 
 if [ "$AZ_BATCH_IS_CURRENT_NODE_MASTER" != "true" ];
 then
@@ -15,6 +15,6 @@ then
     echo "AZ_BATCH_NODE_LIST $AZ_BATCH_NODE_LIST"
     nodes_list=($(echo "$AZ_BATCH_NODE_LIST" | tr ';' ' '))
     echo "nodes_list ${nodes_list[0]} ${nodes_list[1]}"
-    wget https://cloudnativetestingprpsa.blob.core.windows.net/test/SampleApp2.jmx
+    wget -q https://cloudnativetestingprpsa.blob.core.windows.net/test/202102191030128944_HighScaleSampleApp1_HighScaleSampleApp.jmx
     ./apache-jmeter-5.4.1/bin/jmeter -n -t SampleApp2.jmx -Jserver.rmi.ssl.disable=true -R ${nodes_list[1]}
 fi
